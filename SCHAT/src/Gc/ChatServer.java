5d3ac746fab30f732a166bfcbf23488ce7b77628
Rony <ronycherian789@gmail.com>
2015-07-15 14:49:43 +0530

    Database added

--- SCHAT/src/Gc/ChatServer.java
+++ SCHAT/src/Gc/ChatServer.java
@@ -1,12 +1,16 @@
 package Gc;
 
-
+import java.sql.*;
 
 import java.io.IOException;
 import java.util.*;
 import javax.websocket.*;
 import javax.websocket.server.*;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @ServerEndpoint(value = "/chat-server",
         subprotocols={"chat"},
@@ -19,13 +23,23 @@ public class ChatServer {
     private Session session;
     private ServerEndpointConfig endpointConfig;
     private Transcript transcript;
-    private static String all="all";
+    private static String c="all";
+    private static int count=0;
+
+    
+   
     @OnOpen
     public void startChatChannel(EndpointConfig config, Session session) {
         this.endpointConfig = (ServerEndpointConfig) config;
         ChatServerConfigurator csc = (ChatServerConfigurator) endpointConfig.getConfigurator();
         this.transcript = csc.getTranscript();
         this.session = session;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");}
+           catch (ClassNotFoundException t)
+           {}
+        
+        
     }
 
     @OnMessage
@@ -50,10 +64,11 @@ public class ChatServer {
     @OnClose
     public void endChatChannel() {
         if (this.getCurrentUsername() != null) {
-                        this.removeUser();
+            
+            this.removeUser();
         }
     }
-
+    
     void processNewUser(NewUserMessage message) {
         String newUsername = this.validateUsername(message.getUsername());
         NewUserMessage uMessage = new NewUserMessage(newUsername);
@@ -63,16 +78,83 @@ public class ChatServer {
             System.out.println("Error signing " + message.getUsername() + " into chat : " + ioe.getMessage());
         } 
         this.registerUser(newUsername);
-        this.broadcastUserListUpdate();
+        
+ try {
+       ResultSet rs = null,ds=null;
+       String z,x;
+            String url1 = "jdbc:mysql://localhost:3306/chat"; 
+            Connection conn1 = DriverManager.getConnection(url1,"root","root"); 
+            Statement st1 = conn1.createStatement(); 
+            Statement st2 = conn1.createStatement(); 
+            String myQuery = "SELECT Message,Recepient FROM Log WHERE User = '"+newUsername+"'";
+            System.out.println(myQuery);
+          rs = st1.executeQuery ("SELECT Message,Recepient FROM Log WHERE User = '"+newUsername+"';");
+             while(rs.next()){
+            	 z=rs.getString("Message");
+            	 x=rs.getString("Recepient");
+            	 ChatUpdateMessage cdm1 = new ChatUpdateMessage(newUsername,z,x);
+             	try {
+                     session.getBasicRemote().sendObject(cdm1);
+                 } catch (IOException | EncodeException ex) {
+                     System.out.println("Error updating a client : " + ex.getMessage());
+                 }  
+            	 
+            	       }
+           
+             ds= st2.executeQuery ("SELECT Message,User FROM Log WHERE Recepient = '"+newUsername+"';");
+             while(ds.next()){
+            	 z=ds.getString("Message");
+            	 x=ds.getString("User");
+            	 ChatUpdateMessage cdm2 = new ChatUpdateMessage(x,z,newUsername);
+             	try {
+                     session.getBasicRemote().sendObject(cdm2);
+                 } catch (IOException | EncodeException ex) {
+                     System.out.println("Error updating a client : " + ex.getMessage());
+                 }  
+            	 
+            	       }
+            
+        } catch (Exception e) { 
+            System.err.println("Got an exception! "); 
+            System.err.println(e.getMessage()); 
+        } 
+  
+        
+       
+       this.broadcastUserListUpdate();
         
     }
 
     void processChatUpdate(ChatUpdateMessage message) {
+    	String g,m,k;
+    	m=message.getUsername();
+    	g=message.getMessage();
+    	k=message.getRec();
+    	
+        
+        try {
+        	
+            String url = "jdbc:mysql://localhost:3306/chat"; 
+            Connection conn = DriverManager.getConnection(url,"root","root"); 
+            Statement st = conn.createStatement(); 
+           st.executeUpdate("INSERT INTO Log(User,Message,Recepient) " + 
+              "VALUE ('"+m+"','"+g+"','"+k+"')"); 
+           
+           
+        } catch (Exception e) { 
+            System.err.println("Got an exception! "); 
+            System.err.println(e.getMessage()); 
+        } 
+
+        
+    	
+    	
+    	
         this.addMessage(message.getMessage(),message.getRec());
             }
 
     void processSignoffRequest(UserSignoffMessage drm) {
-        
+       
         this.removeUser();   
     }
     
@@ -81,6 +163,7 @@ public class ChatServer {
     }
     
     private void registerUser(String username) {
+    	System.out.println("Inside registration");
         session.getUserProperties().put(USERNAME_KEY, username);
         this.updateUserList();
     }
@@ -129,14 +212,17 @@ public class ChatServer {
         }
     }
 
+   
+   
+        
     private void broadcastTranscriptUpdate() {
-    	String presentRecepient=this.transcript.getRec();
-        String checkUserName;
-    	if(all.equals(presentRecepient))
+    	String v=this.transcript.getRec();
+        String g;
+    	if(c.equals(v))
     	{
         for (Session nextSession : session.getOpenSessions()) {
         	
-            ChatUpdateMessage cdm = new ChatUpdateMessage(this.transcript.getLastUsername(), this.transcript.getLastMessage());
+            ChatUpdateMessage cdm = new ChatUpdateMessage(this.transcript.getLastUsername(), this.transcript.getLastMessage(),this.transcript.getRec());
             
             try {
                 nextSession.getBasicRemote().sendObject(cdm);
@@ -146,10 +232,10 @@ public class ChatServer {
         }}
     	else
     		{for (Session nextSession : session.getOpenSessions()) {
-            	
-                ChatUpdateMessage cdm = new ChatUpdateMessage(this.transcript.getLastUsername(), this.transcript.getLastMessage());
-                checkUserName=(String) nextSession.getUserProperties().get(USERNAME_KEY);
-                if(presentRecepient.equals(checkUserName))
+            	String check=this.transcript.getRec();
+                ChatUpdateMessage cdm = new ChatUpdateMessage(this.transcript.getLastUsername(), this.transcript.getLastMessage(),this.transcript.getRec());
+                g=(String) nextSession.getUserProperties().get(USERNAME_KEY);
+                if(v.equals(g))
                 		{try {
                             nextSession.getBasicRemote().sendObject(cdm);
                         } catch (IOException | EncodeException ex) {
@@ -163,5 +249,6 @@ public class ChatServer {
         this.transcript.addEntry(this.getCurrentUsername(), message,rec);
         this.broadcastTranscriptUpdate();
     }
+   
 
 }
--- SCHAT/src/Gc/ChatUpdateMessage.java
+++ SCHAT/src/Gc/ChatUpdateMessage.java
@@ -2,20 +2,13 @@ package Gc;
 
 public class ChatUpdateMessage extends StructuredMessage {
     
-    public ChatUpdateMessage(String username, String message,String rec) {
+    public ChatUpdateMessage(String username, String message,String rec ) {
         super(ChatMessage.CHAT_DATA_MESSAGE);
         super.dataList.add(username);
         super.dataList.add(message);
         super.dataList.add(rec);
-      
-    };
-    public ChatUpdateMessage(String username, String message) {
-        super(ChatMessage.CHAT_DATA_MESSAGE);
-        super.dataList.add(username);
-        super.dataList.add(message);
-        
-      
-    };
+    }
+    
     public String getUsername() {
         return (String) super.getList().get(0);
     }
